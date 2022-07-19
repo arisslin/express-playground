c8549eadba45382ad259d66c4112b7a879b131e5
Andreas Rissling <a.rissling@steinberg.de>
2022-07-19 11:14:59 +0200

    Refactor server code

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import { Request, Response } from 'express';
 const cors = require('cors');
 
 const app = express();
@@ -9,25 +10,28 @@ app.use(cors());
 app.use('/post-json/', express.json());
 
 app.post('/post-json/', (req, res) => {
-  console.log('POST Request: ' + req.url);
-  console.log(req.body);
-
-  res.type('text');
-  res.status(200);
-  res.send('POST JSON DONE!');
+  logPostRequest(req);
+  sendResponse200(res, 'POST JSON DONE!');
 });
 
 app.use('/post-raw/', express.raw());
 
 app.post('/post-raw/', (req, res) => {
-  console.log('POST Request: ' + req.url);
-  console.log(req.body.toString());
-
-  res.type('text');
-  res.status(200);
-  res.send('POST RAW DONE!');
+  logPostRequest(req);
+  sendResponse200(res, 'POST RAW DONE!');
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+function logPostRequest(req: Request): void {
+  console.log('POST Request: ' + req.url);
+  console.log(req.body);
+}
+
+function sendResponse200(res: Response, body: string): void {
+  res.type('text');
+  res.status(200);
+  res.send(body);
+}
